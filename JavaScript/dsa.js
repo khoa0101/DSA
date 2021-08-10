7bf3d15b045045906d7b461959d0f71b2e39c8e3
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-08-09 19:34:06 -0700

    Add twoSum js

---
+++ JavaScript/dsa.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = new Map() // key = target - currentValue, value - current index
+
+  for (let i = 0; i < nums.length; i++) {
+    const val = nums[i]
+    const difference = target - val
+
+    if (map.has(val)) {
+      return [map.get(val), i]
+    }
+
+    map.set(difference, i)
+  }
+
+};
