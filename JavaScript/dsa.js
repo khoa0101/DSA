4121ae3af5eabb90f8ec11a66e131621c5b8fa4e
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-08-09 22:22:11 -0700

    Add reverseInt js

--- JavaScript/dsa.js
+++ JavaScript/dsa.js
@@ -1,3 +1,4 @@
+// Return the target sums of two numbers in the argument array
 var twoSum = function (nums, target) {
   const map = new Map() // key = target - currentValue, value - current index
 
@@ -12,4 +13,11 @@ var twoSum = function (nums, target) {
     map.set(difference, i)
   }
 
+};
+
+// Return the reverse of the interger, if the reverse interger exceeds the signed 32-bit range, it will return 0
+var reverse = function(x) {
+    var y = parseInt(x.toString().split('').reverse().join(''))
+    if (y >= 2**31-1 || -y <= -(2**31)) return 0;
+    return x < 0 ?  -y : y  
 };
