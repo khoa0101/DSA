b3a34952eba139864950ab40a7beaf2aef86a4a5
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-08-12 16:24:20 -0700

    Add removeElement js

--- JavaScript/dsa.js
+++ JavaScript/dsa.js
@@ -164,3 +164,14 @@ var removeDuplicates = function(nums) {
     
     return nums.length - count;
 };
+
+var removeElement = function(nums, val) {
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] == val){
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    
+    return nums.length;
+};
