9ff09d99f3844af4751a7720e5bff5d1083216c7
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-08-12 16:52:33 -0700

    Add searchInsert js

--- JavaScript/dsa.js
+++ JavaScript/dsa.js
@@ -189,3 +189,19 @@ var strStr = function(haystack, needle) {
         return str[0].length;
     }
 };
+
+var searchInsert = function(nums, target) {
+    if (target < nums[0]) return 0;
+    if (target > nums[nums.length - 1]) return nums.length;
+    let start = 0, mid = 0, end = nums.length - 1;
+    while (start < end) {
+        mid = Math.floor((end + start) / 2);
+        if (nums[mid] > target)
+            end = mid;
+        else if (nums[mid] < target)
+            start = mid + 1;
+        else
+            break;
+    }
+    return nums[mid] >= target ? mid : mid + 1
+};
