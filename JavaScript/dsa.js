b8b6020dd41c379123168f483eac31f77a77e7a1
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-08-18 23:33:31 -0700

    Add merge js

--- JavaScript/dsa.js
+++ JavaScript/dsa.js
@@ -299,3 +299,10 @@ var deleteDuplicates = function(head) {
     
     return head;
 };
+
+var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m);
+    nums2.splice(n);
+    nums1.push(...nums2);
+    nums1.sort((a, b) => a - b);
+};
