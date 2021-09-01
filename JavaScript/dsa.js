44622749251e9edf77b0612757ae3802d66e8c3e
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-08-31 21:09:03 -0700

    Add minStack js

--- JavaScript/dsa.js
+++ JavaScript/dsa.js
@@ -664,3 +664,40 @@ var getIntersectionNode = function(headA, headB) {
     return null;
     
 };
+
+var MinStack = function() {
+    this.stack = []
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    return this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    let min = this.stack[0];
+    for (let n of this.stack){
+        if (n < min) min = n;
+    }
+    return min;
+};
