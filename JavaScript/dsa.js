5a3b9e7adca5dccb8ee446c0de01e0a43bffa8e3
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-10-20 21:55:16 -0700

    Add isUgly

--- JavaScript/dsa.js
+++ JavaScript/dsa.js
@@ -1126,3 +1126,16 @@ var addDigits = function(num) {
     if (num < 10) return num;
     return num - (Math.floor(num / 9)) * 9 || 9;
 };
+
+var isUgly = function(n) {
+    if (n < 1) return false;
+    
+    while (n % 2 === 0 || n % 3 === 0 || n % 5 === 0){
+        if (n % 2 === 0) n /= 2;
+        if (n % 3 === 0) n /= 3;
+        if (n % 5 === 0) n /= 5; 
+    }
+    
+    
+    return n === 1;
+};
