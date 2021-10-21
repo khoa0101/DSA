b275baf4db10ccdeee06d78f4407ff2b01aa5c97
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-10-20 22:12:15 -0700

    Add isBadVersion js

--- JavaScript/dsa.js
+++ JavaScript/dsa.js
@@ -1150,3 +1150,23 @@ var missingNumber = function(nums) {
     
     return nums.length; 
 };
+
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let l = 1, r = n;
+        
+        while (l <= r){
+            const mid = Math.floor((l + r)/ 2);
+            if (!isBadVersion(mid)){
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    };
+};
