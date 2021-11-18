0b60768ec677202f02c30d85d90c432f70646a5a
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-11-18 15:54:16 -0800

    Add toHex js

--- JavaScript/dsa.js
+++ JavaScript/dsa.js
@@ -1372,3 +1372,19 @@ var sumOfLeftLeaves = function(root) {
     
     return sum;
 };
+
+var toHex = function(num) {
+    let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+    let str = "";
+    
+    if (num < 0) num = num + 2**32
+    while (num > 15) {
+        str = digits[num % 16] + str
+        num = Math.floor(num / 16)
+    }
+    
+    str = digits[num % 16] + str
+    if (str[0] === "0" && str.length > 1) str = str.slice(1) 
+    return str
+    
+};
