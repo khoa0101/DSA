2061038461b81643d5b3787b3521b50b7cb93a21
Khoa Nguyen <70730501+khoa0101@users.noreply.github.com>
2022-10-31 20:27:44 -0700

    Time: 129 ms (13.72%), Space: 46.3 MB (18.44%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+const reverseStr = function(s, k){
+    const arr = s.split('');
+    
+    for (let i = 0; i < arr.length; i += 2 * k) {
+        let left = i;
+        let right = i + k - 1;
+        
+        while (left < right) {
+            if (arr[right] === undefined) {
+                right--;
+                continue;
+            }
+            
+            [arr[left], arr[right]] = [arr[right], arr[left]];
+            left++;
+            right--;
+        }
+    }
+    
+    return arr.join('');
+};
